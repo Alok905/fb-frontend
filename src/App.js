e3d84778900b4b5738820154ae5fcc32fed5facb
Alok905 <alokranjanjoshi07567@gmail.com>
2022-12-19 20:11:42 +0530

    resources added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { LiveVideo } from "./svg";
 
 function App() {
+  const get = async () => {
+    const res = await fetch("http://localhost:8000");
+  };
+  get();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div> Welcome to Frontend </div>
+    </>
   );
 }
 
